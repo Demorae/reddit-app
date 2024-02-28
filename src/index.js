8ab9f683595c4b6d55348ef3d85453e4e7e47b56
Demorae <142933169+Demorae@users.noreply.github.com>
2024-02-28 14:47:07 +0900

    altered postlist.js and index.html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 // src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
 import { Provider } from 'react-redux';
 import store from './store/store';
 import App from './containers/App';
 
-ReactDOM.render(
+// Use createRoot instead of ReactDOM.render
+const root = createRoot(document.getElementById('root'));
+
+// Render your app inside the root
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
